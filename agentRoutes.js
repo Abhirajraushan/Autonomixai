593edc57594b47955266671bfbc64db444b3b13f
Abhirajraushan <84666932+Abhirajraushan@users.noreply.github.com>
2025-06-06 19:48:39 +0530

    Delete agentRoutes.js

--- agentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerAgent } = require('../controllers/agentController');
-
-router.post('/create', registerAgent); // '/create' maps to /api/agents/create
-
-module.exports = router;
