d2e47694958ee47817b7afeaf8c699a24bc6e064
Abhirajraushan <84666932+Abhirajraushan@users.noreply.github.com>
2025-07-10 10:36:35 +0530

    Update agentRoutes.js

--- Backend/agentRoutes.js
+++ Backend/agentRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const { registerAgent } = require('../controllers/agentController');
-
-router.post('/create', registerAgent); // '/create' maps to /api/agents/create
-
+router.post('/create', registerAgent); // '/here we create' maps to /api/agents/create
 module.exports = router;
